ee6b906401cfff3dd1f58560a5921f56e311136f
Ik4ri <ik4ri.dump@gmail.com>
2026-05-12 20:38:44 +0800

    facial recognition feature

---
+++ amsirs-web/lib/face/loadModels.ts
@@ -0,0 +1,11 @@
+import * as faceapi from "face-api.js";
+
+export async function loadModels() {
+  const MODEL_URL = "/models";
+
+  await Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+    faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+    faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+  ]);
+}
